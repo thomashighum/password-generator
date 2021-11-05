73141ddb642140e07a85f73b61a12488c5e94d6e
Tom Highum <tomhighum@rocketmail.com>
2021-11-05 16:47:40 -0600

    cleaned up a bit

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function generatePassword() {
 // prompt for length 
     var passwordLength = prompt("How long would you like your password to be? \nBetween 8 and 128 characters.");
     // limit input to between 8 and 128.
-    if (passwordLength < 8 || passwordLength > 128) {
+    if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
       // if incorrect value alert and reset
       alert("Invalid input, \nPlease input a number between 8 and 128.");
       generatePassword();
@@ -39,7 +39,7 @@ function generatePassword() {
           alert("You will need to select at least one character type.")
           characterTypes()
         } else {
-          //  var arrayChoices = [].concat(lowerCase, upperCase, numbers, specChars)
+          
           function passwordRandom() { // generate the password to length and using types
             if ( typeChoices.lowerCaseConfirm){
               emptyArray = emptyArray.concat(lowerCase)
@@ -53,12 +53,12 @@ function generatePassword() {
             if ( typeChoices.specCharsConfirm){
               emptyArray = emptyArray.concat(specChars)
             }
-            // emptyArray.join('');
+            
             var newPass = ""
             for (i = 0; i < passwordLength; i++) {
               newPass += emptyArray[Math.floor(Math.random() * emptyArray.length)];
             };
-
+            
 
             console.log(newPass)
             console.log(emptyArray)
@@ -83,3 +83,4 @@ function generatePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
