109b04aad26a3879286d3a85e489bc43ee204f80
Tom Highum <tomhighum@rocketmail.com>
2021-11-05 12:52:44 -0600

    confirm for atleast one type, types made into object, added number data set, starting on concat multiple data set arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,39 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Data sets
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ]
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ]
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var specChars = ["!", "#", "$", "%", "&", "*", "[", "]", "{", "}", "@", "~", "=", "+", "-", "_", "(", ")", "^", ".", "/", "|", ";", ":"]
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-
-  function generatePassword() {
-// prompt for length ** NEED to limit input to between 8 and 128.
-    var passwordLength = prompt("How long would you like your password to be? \nBetween 8 and 128 characters.");
-    if (passwordLength < 8 || passwordLength > 128) {
-      alert("Invalid input, \nPlease input a number between 8 and 128.");
-      generatePassword()
-    } else {
-      function characterTypes() {
-          //confirm for content types ** NEED to ensure atleast one is true
-        var lowerCaseConfirm = confirm("Do you want to include lowercase letters?")
-        var upperCaseConfirm = confirm("Do you want to include uppercase letters?")
-        var numbersConfirm = confirm("Do you want to include numbers?")
-        var specCharsConfirm = confirm("Do you want to include special characters?")
-      }
-    }
-
-
-
-
-  }
-  // Data sets
-  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ]
-  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ]
-  // var numbers = 
-  var specChars = ["!", "#", "$", "%", "&", "*", "[", "]", "{", "}", "@", "~", "=", "+", "-", "_", "(", ")", "^", ".", "/", "|", ";", ":"]
-
-  alert(length + lowerCase + upperCase + numbers + specChars)
 
 
   passwordText.value = password;
@@ -41,8 +19,50 @@ function writePassword() {
 }
 
 
+function generatePassword() {
+// prompt for length 
+    var passwordLength = prompt("How long would you like your password to be? \nBetween 8 and 128 characters.");
+    // limit input to between 8 and 128.
+    if (passwordLength < 8 || passwordLength > 128) {
+      // if incorrect value alert and reset
+      alert("Invalid input, \nPlease input a number between 8 and 128.");
+      generatePassword();
+    } else {
+      function characterTypes() {
+        var typeChoices = {  //confirm for content types
+          lowerCaseConfirm: confirm("Do you want to include lowercase letters?"),
+          upperCaseConfirm: confirm("Do you want to include uppercase letters?"),
+          numbersConfirm: confirm("Do you want to include numbers?"),
+          specCharsConfirm: confirm("Do you want to include special characters?"),
+        };
+        if (// confirm atleat one choice made
+          typeChoices.lowerCaseConfirm === false &&
+          typeChoices.upperCaseConfirm === false &&
+          typeChoices.numbersConfirm === false &&
+          typeChoices.specCharsConfirm === false 
+        ){ // if none given alert and reset, NEED -- Continue or not
+          alert("You will need to select at least one character type.")
+          characterTypes()
+        } else {
+          function passwordRandom() { // generate the password to length and using types
+              var arrayChoices = [].concat()
+          }
+        }
+      }
+    }
 
 
+
+    // Math.floor(Math.random() * passwordLength.length)
+    
+
+  }
+
+
+  
+
+
+  // alert(length + lowerCase + upperCase + numbers + specChars)
 
 
 // create password data sets
